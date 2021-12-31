73f54d142913ea6cad8a935544000899e79b1fd3
matthes-me <maik@matthes.me>
2021-12-31 14:58:26 +0100

    silvester

---
+++ groundzeroset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="groundzeroset" tilewidth="32" tileheight="32" tilecount="2048" columns="64">
+ <image source="../../../rc3world/rc3world/engel-kittchen2021/tilesets/groundzeroset.png" width="2048" height="1024"/>
+</tileset>
