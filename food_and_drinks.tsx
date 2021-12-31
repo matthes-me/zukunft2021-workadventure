cc28a1ceed538a03d5148f21490454bd89528817
matthes-me <maik@matthes.me>
2021-12-31 13:53:11 +0100

    start

---
+++ food_and_drinks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="food_and_drinks" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
+ <image source="food_and_drinks.png" width="512" height="2048"/>
+</tileset>
