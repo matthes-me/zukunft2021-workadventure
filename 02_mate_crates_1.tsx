0605841eba7c97e63d7cb86399e3e75870145c72
matthes-me <maik@matthes.me>
2021-12-31 15:35:58 +0100

    neu

--- 02_mate_crates_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="02_mate_crates_1" tilewidth="32" tileheight="32" tilecount="3" columns="1">
- <image source="02_mate_crates_1.png" width="32" height="96"/>
-</tileset>
